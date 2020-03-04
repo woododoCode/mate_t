4e9329b739424e3111f7fa4e4a22fd0dee902526
woododoCode <woododoCode@users.noreply.github.com>
2020-03-04 11:59:42 +0200

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("We will not write Hello world");
+        System.out.println("Hello mates!");
     }
 }
